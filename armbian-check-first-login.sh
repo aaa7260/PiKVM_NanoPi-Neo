04b0eeec01824dbae8ab555ffb05c0fb48fcc7ab
Road <496814664@qq.com>
2023-06-19 15:41:52 +0800

    Create armbian-check-first-login.sh

---
+++ armbian-check-first-login.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Copyright (c) Authors: https://www.armbian.com/authors
+#
+# This file is licensed under the terms of the GNU General Public
+# License version 2. This program is licensed "as is" without any
+# warranty of any kind, whether express or implied.
+
+# File in packages/bsp/common/usr/lib/armbian/armbian-firstlogin
+
+if [ -e /root/kvmd-armbian/.part1_install_yet ]; then
+	bash /root/kvmd-armbian/install.sh
+fi
+
+# First login as root?
+if [ -w /root/ -a -f /root/.not_logged_in_yet ]; then
+	bash /usr/lib/armbian/armbian-firstlogin
+fi
